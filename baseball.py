31095d906125263987b1893f217747a9443c6887
helloyj12 <40688289+helloyj12@users.noreply.github.com>
2023-02-01 17:12:54 +0900

    Update baseball.py

--- baseball.py
+++ baseball.py
@@ -103,42 +103,42 @@ def  plotting_demo():
     st.dataframe(money)
        
 
- def bar_chart():
+def bar_chart():
 
-     url = "https://sports.news.naver.com/kbaseball/record/index?category=kbo&year="
+    url = "https://sports.news.naver.com/kbaseball/record/index?category=kbo&year="
 
-     years = ['2015', '2016','2017', '2018', '2019', '2020', '2021', '2022' ]
+    years = ['2015', '2016','2017', '2018', '2019', '2020', '2021', '2022' ]
 
-     baseball = pd.DataFrame([]) 
+    baseball = pd.DataFrame([]) 
 
-     for i in years: 
-         df1 = pd.read_html(url+i)[0]
-         df1['년도'] =  i 
-         baseball = pd.concat([baseball, df1], axis=0)
+    for i in years: 
+        df1 = pd.read_html(url+i)[0]
+        df1['년도'] =  i 
+        baseball = pd.concat([baseball, df1], axis=0)
         
      # baseball.팀.replace({'두산':'Dusan','삼성':'SS','키움':'KU','한화': 'HH','롯데':'Lotte','넥센':'NecSen'}, inplace=True)
     
-     option = st.selectbox(
-         'How would you like to choice year ?',
-         ('2015', '2016','2017', '2018', '2019', '2020', '2021', '2022'))
+    option = st.selectbox(
+        'How would you like to choice year ?',
+        ('2015', '2016','2017', '2018', '2019', '2020', '2021', '2022'))
 
-     st.write('You selected:', option)
+    st.write('You selected:', option)
 
-     baseball_graph = baseball[baseball.년도==option]
-     x = baseball_graph.팀
-     y = baseball_graph.승률
+    baseball_graph = baseball[baseball.년도==option]
+    x = baseball_graph.팀
+    y = baseball_graph.승률
     
-     fig, ax = plt.subplots(figsize=(12,8))
+    fig, ax = plt.subplots(figsize=(12,8))
 
-     colors = ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7' ,'C8', 'C9', 'C10' ]
-     plt.bar(x, y, color= colors ) 
+    colors = ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7' ,'C8', 'C9', 'C10' ]
+    plt.bar(x, y, color= colors ) 
 
-     for i, v in enumerate( y ):
-         plt.text(i-0.4, v+0.01, v)
+    for i, v in enumerate( y ):
+        plt.text(i-0.4, v+0.01, v)
 
-     plt.title( "year korea baseball winrate data", position=(0.5,1.1))
-     st.pyplot(fig)
-     st.dataframe(baseball_graph)    
+    plt.title( "year korea baseball winrate data", position=(0.5,1.1))
+    st.pyplot(fig)
+    st.dataframe(baseball_graph)    
 
 with st.form(key ='Form1'):
     with st.sidebar:
